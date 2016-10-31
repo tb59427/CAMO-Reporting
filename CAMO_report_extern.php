eb530597d4590ad7784e8fbb8041cc42d4b31a46
tb59427 <tb@pobox.com>
2016-10-31 17:28:47 +0100

    Added config file - so no config detail left in the code other than the base path (where CAMOreport is located)

--- CAMO_report_extern.php
+++ CAMO_report_extern.php
@@ -11,11 +11,16 @@
     // Am besten lässt man dieses Skript Nachts (nach Sunset und Sunset-Bier) laufen. Wir lassen es um 23:45 laufen
     //
     // To do's:
-    // - Konfigurationsdaten aus dem Skript entfernen und in eine eigene Konfig-Datei auslagern
     // - Datum für das reported werden soll konfigurierbar machen
-    // Version 0.1 - 28.10.16 erste Version
+    // Versionen
+    // 0.1 - 28.10.16 erste Version
+    // 0.2 - 31.10.16 umgestellt auf Konfigurationsdatei - keine IDs etc mehr im Code
     //
     // (c) Torsten Beyer, 2016
+    
+    $BASE_PATH = '/homepages/25/d558414169/htdocs/vereinsflieger/';
+    
+    $CONFIG_FILE = $BASE_PATH . "CAMOReport/CAMO.cfg";
     
     $aBBentry = array (
                 "date" => "",
@@ -26,35 +31,26 @@
                 "blocktime" => 0,
                 "landingcount" => 0
     );
-    
-    //
-    // !!!Ändern!!!: hier die Liste der Vereinsflugzeuge eintragen - wenn bei Euch nur Vereinsflugzeuge fliegen, kannst man die markierte Stelle in der for Schleife umbauen
-    //
-    $LSVplanes = array ("D-EZIC","D-EAUO","D-KNAE","D-MSEF","D-2991","D-3695","D-8710","D-7655","D-0326", "D-0632","D-4239");
     $Flights = array();
     
-    // !!!Ändern!!!: Login/Password für Anmeldung bei Vereinsflieger
-    $VFlogin = "irgendwer@sonstwo.com";
-    $VFpasswd = "geheim";
-    //
-    // !!!Ändern!!!: hier die absoluten Pfade zu den beiden inkludierten Klassen-Definitionen einfügen.
-    // Das VereinsfliegerRestInterface gibt's zum herunterladen in vereinsflieger.de (Administration, dann ? und dann rumklicken)
-    // PHPMailer gibt's auf git
-    // beide irgendwo hinladen und die Pfade unten anpassen
-    //
-    require_once('/homepages/25/d558414169/htdocs/vereinsflieger/CAMOReport/VereinsfliegerRestInterface.php');
-    require '/homepages/25/d558414169/htdocs/vereinsflieger/PHPMailer/PHPMailerAutoload.php';
+    
+    require_once($BASE_PATH . 'CAMOReport/VereinsfliegerRestInterface.php');
+    require $BASE_PATH . 'PHPMailer/PHPMailerAutoload.php';
+    
+    $configuration = parse_ini_file ($CONFIG_FILE, 1);
+    $CAMOPlanes = explode (",",$configuration[verein][flugzeuge]);
     
     date_default_timezone_set ( "UTC");
     $a = new VereinsfliegerRestInterface();
     
     
         
-    $result = $a->SignIn($VFlogin,$VFpasswd,0);
+    $result = $a->SignIn($configuration[vereinsflieger][login_name],$configuration[vereinsflieger][passwort],0);
 
     if ($result) {
         
         $return = $a->GetFlights_today();
+        // $return = $a->GetFlights_date ("2016-10-30");
         if ($return) {
             
             
@@ -68,12 +64,7 @@
             // alle Flüge sind in aResponse
             //
             
-            // gab's überhaupt Flüge an dem Tag?
-            //
             if ($no_Flights > 0) {
-                
-                // jo, es gab Flüge  also durch $aResponse durchhangeln Flug für Flug
-                //
                 for ($i=0; $i<$no_Flights;$i++) {
                     
                     $id = $aResponse[$i]["callsign"];
@@ -81,14 +72,8 @@
                     $start = new DateTime($aResponse[$i]["departuretime"]);
                     $ende = new DateTime ($aResponse[$i]["arrivaltime"]);
                     
-                    //
-                    // Diese if Abfrage kann weg, wenn vereinsflieger nur Flüge von Flugzeugen meldet, die in der CAMO sind
-                    // bei uns ist das aber nicht so, da wir säckeweise Privatflugzeuge haben
-                    //
-                    if (in_array($id, $LSVplanes)) {
+                    if (in_array($id, $CAMOPlanes)) {
                         
-                        // haben wir schon einen Eintrag für dieses Flugzeug?
-                        // Wenn ja, Daten hochzählen und schauen, ob dieser Flug vielleicht der früheste Start oder die letzte Landung war.
                         if (array_key_exists ($id,$Flights)) {
                             $Flights[$id]["flighttime"] += intval ($aResponse[$i]["flighttime"]);
                             $Flights[$id]["landingcount"] += intval ($aResponse[$i]["landingcount"]);
@@ -107,9 +92,6 @@
                             
                             
                         }
-                        //
-                        // Nee, den gab's noch nicht - neu anlegen
-                        //
                         else {
                             
                             $Flights[$id] = $aBBentry;
@@ -125,10 +107,7 @@
                     }
                 }
                 
-                //
-                // so, die Arbeit ist getan. Jetzt wegschreiben den Kram
-                // Hier könnte man auch eine csv generieren (kann php off the shelf)
-                //
+                
                 $fp = fopen("fluege.txt","w");
                 $subject = sprintf ("CAMO Report %s:\n", $datum);
                 
@@ -140,10 +119,9 @@
                 }
                 fclose ($fp);
                 
-                // So jetzt mailen den ganzen Kram
                 //Create a new PHPMailer instance
                 $mail = new PHPMailer;
-                //Tell PHPMailer to use SendMail !!! ggfs. Ändern, je nachdem, welches Mailsystem auf dem Server installiert ist
+                //Tell PHPMailer to use SendMail
                 $mail->IsSendmail();
                 //Enable SMTP debugging
                 // 0 = off (for production use)
@@ -152,8 +130,8 @@
                 $mail->SMTPDebug = 0;
                 //Ask for HTML-friendly debug output
                 $mail->Debugoutput = 'html';
-                //Set the hostname of the mail server !!!Ändern!!!
-                $mail->Host = gethostbyname('smtp.somwhere.com');
+                //Set the hostname of the mail server
+                $mail->Host = gethostbyname($configuration[mail][smtp_server]);
                 // if your network does not support SMTP over IPv6
                 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
                 $mail->Port = 587;
@@ -161,17 +139,23 @@
                 $mail->SMTPSecure = 'tls';
                 //Whether to use SMTP authentication
                 $mail->SMTPAuth = true;
-                //Username to use for SMTP authentication - use full email address for mailservice !!!Ändern!!!
-                $mail->Username = "EinUserName";
-                //Password to use for SMTP authentication !!!Ändern!!!
-                $mail->Password = "geheim";
-                //Set who the message is to be sent from !!!Ändern
-                $mail->setFrom('jemand@deinclub.de', 'DeinClub e.V.');
+                //Username to use for SMTP authentication - use full email address for gmail
+                $mail->Username = $configuration[mail][smtp_login];
+                //Password to use for SMTP authentication
+                $mail->Password = $configuration[mail][smtp_passwd];
+                //Set who the message is to be sent from
+                $mail->setFrom($configuration[mail][from_address], $configuration[mail][from_name]);
                 //Set an alternative reply-to address
-                $mail->addReplyTo('jemand@deinclub.de', 'DeinClub e.V.');
-                //Set who the message is to be sent to !!!Ändern!!! - wenn die Mail an mehrere gehen soll einfach mehrere Aufrufe an $mail->addAdress machen mit jeweils andern Adressen
-                $mail->addAddress('jemand@deinecamo.com', 'Deine CAMO');
+                $mail->addReplyTo($configuration[mail][from_address], $configuration[mail][from_name]);
+                //Set who the message is to be sent to
                 
+                $receivers = explode (",", $configuration[mail][receivers]);
+                foreach ($receivers as $receiver) {
+                  
+                    $receiver_details = explode (":", $receiver);
+                    $mail->addAddress($receiver_details[0], $receiver_details[1]);
+                    
+                }
                 //Set the subject line
                 $mail->Subject = $subject;
                 $mail->Body = "Anbei der " . $subject . "\n\n";
